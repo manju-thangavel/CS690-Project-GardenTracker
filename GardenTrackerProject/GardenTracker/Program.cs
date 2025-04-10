df094005a909809bb3b46a1d92ed9e1f31d19768
manju-thangavel <srimanjunath.thangavel@hotmail.com>
2025-04-10 19:00:34 -0400

    Update Program.cs

--- GardenTrackerProject/GardenTracker/Program.cs
+++ GardenTrackerProject/GardenTracker/Program.cs
@@ -1,58 +1,152 @@
 using GardenTracker.Management;
 using GardenTracker.Models;
 using System;
-using Spectre.Console;
+using System.Collections.Generic;
 
-namespace GardenTracker;
-
-public class Program
+namespace GardenTracker
 {
-    static void Main(string[] args)
+    public class Program
     {
-        var userManagement = new UserManagement();
-        if (!userManagement.Login())
+        static void Main(string[] args)
         {
-            AnsiConsole.Markup("[bold red]Login failed. Exiting application.[/]");
-            return;
-        }
+            var consoleUI = new ConsoleUI();
+            var userManagement = new UserManagement();
 
-        var plantsManagement = new PlantsManagement();
-        var activityManagement = new ActivityManagement();
-
-        bool exit = false;
-        while (!exit)
-        {
-            AnsiConsole.MarkupLine("[bold cyan]\nHi there! Please choose an option:[/]");
-            var choice = AnsiConsole.Prompt(
-                new SelectionPrompt<string>()
-                    .Title("[bold green]Select an option:[/]")
-                    .AddChoices(new[]
-                    {
-                        "Log Activity",
-                        "View Reminders",
-                        "Manage Plants",
-                        "View History",
-                        "Exit"
-                    }));
-
-            switch (choice)
+            if (!userManagement.Login())
             {
-                case "Log Activity":
-                    activityManagement.LogActivity(plantsManagement.LoadPlants());
-                    break;
-                case "Manage Plants":
-                    plantsManagement.ManagePlants();
-                    break;
-                case "Exit":
-                    AnsiConsole.MarkupLine("[bold yellow]You entered option to Exit.[/]");
-                    exit = true;
-                    break;
-                default:
-                    AnsiConsole.MarkupLine("[bold red]Invalid entry. Try again.[/]");
-                    break;
+                consoleUI.PrintMessage("Login failed. Exiting application.", "red");
+                return;
+            }
+
+            var plantsManagement = new PlantsManagement();
+            var activityService = new ActivityService();
+            var activityManagement = new ActivityManagement(activityService);
+            var reminderManagement = new ReminderManagement(activityService);
+            var historyManagement = new HistoryManagement(activityService);
+
+            bool exit = false;
+            while (!exit)
+            {
+                var choice = consoleUI.PromptSelection("Select an option:", new[]
+                {
+                    "Log Activity",
+                    "View Reminders",
+                    "View History",
+                    "Manage Plants",
+                    "Exit"
+                });
+
+                switch (choice)
+                {
+                    case "Log Activity":
+                        var plants = plantsManagement.GetPlants();
+                        if (plants.Count == 0)
+                        {
+                            consoleUI.PrintMessage("No plants available. Please add plants first.", "red");
+                            break;
+                        }
+
+                        var selectedPlant = consoleUI.PromptSelection("Select a plant:", plants, p => p.Name);
+                        var activityType = consoleUI.PromptSelection("Select an activity:", Enum.GetValues<ActivityType>());
+                        var note = consoleUI.AskString("Enter your custom note for this activity:");
+                        var setReminder = consoleUI.Confirm("Do you want to set a reminder for this activity?");
+                        DateTime? reminderDate = null;
+
+                        if (setReminder)
+                        {
+                            int reminderDays = consoleUI.AskInt("Enter number of days to be reminded in:");
+                            reminderDate = DateTime.UtcNow.AddDays(reminderDays);
+                        }
+
+                        activityManagement.LogActivity(selectedPlant, activityType, note, reminderDate);
+                        consoleUI.PrintMessage("Activity logged successfully!");
+                        break;
+
+                    case "View Reminders":
+                        int days = consoleUI.AskInt("Enter the number of days to view reminders:");
+                        var reminders = reminderManagement.GetReminders(days);
+                        if (reminders.Count == 0)
+                        {
+                            consoleUI.PrintMessage("No reminders for the given period!", "red");
+                        }
+                        else
+                        {
+                            foreach (var reminder in reminders)
+                            {
+                                consoleUI.PrintMessage($"{reminder.Plant.Name} - {reminder.Type} on {reminder.ReminderDate.Value:yyyy-MM-dd} - Note: {reminder.Note}", "yellow");
+                            }
+                        }
+                        break;
+
+                    case "View History":
+                        var allPlants = plantsManagement.GetPlants();
+                        if (allPlants.Count == 0)
+                        {
+                            consoleUI.PrintMessage("No plants available to view history.", "red");
+                            break;
+                        }
+
+                        var plantToView = consoleUI.PromptSelection("Select a plant to view history:", allPlants, p => p.Name);
+                        int historyDays = consoleUI.AskInt("Enter the number of days to view history:");
+                        var history = historyManagement.GetHistory(plantToView, historyDays);
+
+                        if (history.Count == 0)
+                        {
+                            consoleUI.PrintMessage("No activities found for the given period!", "red");
+                        }
+                        else
+                        {
+                            consoleUI.PrintMessage("Activity History:", "cyan");
+                            foreach (var activity in history)
+                            {
+                                consoleUI.PrintMessage($"{activity.Date.ToUniversalTime():yyyy-MM-dd HH:mm:ss} - {activity.Type} - Note: {activity.Note}", "yellow");
+                            }
+                        }
+                        break;
+
+                    case "Manage Plants":
+                        var plantChoice = consoleUI.PromptSelection("Manage Plants:", new[] { "Add Plant", "Remove Plant" });
+                        if (plantChoice == "Add Plant")
+                        {
+                            var plantName = consoleUI.AskString("Enter plant name to add:");
+                            try
+                            {
+                                plantsManagement.AddPlant(plantName);
+                                consoleUI.PrintMessage("Plant added successfully!");
+                            }
+                            catch (Exception ex)
+                            {
+                                consoleUI.PrintMessage(ex.Message, "red");
+                            }
+                        }
+                        else if (plantChoice == "Remove Plant")
+                        {
+                            var plantsToRemove = plantsManagement.GetPlants();
+                            if (plantsToRemove.Count == 0)
+                            {
+                                consoleUI.PrintMessage("No plants to remove.", "red");
+                                break;
+                            }
+
+                            var plantToRemove = consoleUI.PromptSelection("Select a plant to remove:", plantsToRemove, p => p.Name);
+                            try
+                            {
+                                plantsManagement.RemovePlant(plantToRemove);
+                                consoleUI.PrintMessage("Plant removed successfully!");
+                            }
+                            catch (Exception ex)
+                            {
+                                consoleUI.PrintMessage(ex.Message, "red");
+                            }
+                        }
+                        break;
+
+                    case "Exit":
+                        consoleUI.PrintMessage("Thank you, Bye!");
+                        exit = true;
+                        break;
+                }
             }
         }
-
-        AnsiConsole.MarkupLine("[bold green]Thank you, Bye![/]");
     }
 }
