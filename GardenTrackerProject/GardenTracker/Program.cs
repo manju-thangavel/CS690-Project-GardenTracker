bbfa417d03fc0b81c89c85f56b72e07413617d9f
manju-thangavel <srimanjunath.thangavel@hotmail.com>
2025-04-10 08:19:09 -0400

    Update Program.cs

--- GardenTrackerProject/GardenTracker/Program.cs
+++ GardenTrackerProject/GardenTracker/Program.cs
@@ -1,54 +1,58 @@
-﻿namespace GardenTracker;
+using GardenTracker.Management;
+using GardenTracker.Models;
+using System;
+using Spectre.Console;
 
-class Program
+namespace GardenTracker;
+
+public class Program
 {
     static void Main(string[] args)
     {
-            User user = new User();
-            if (!user.Login())
+        var userManagement = new UserManagement();
+        if (!userManagement.Login())
+        {
+            AnsiConsole.Markup("[bold red]Login failed. Exiting application.[/]");
+            return;
+        }
+
+        var plantsManagement = new PlantsManagement();
+        var activityManagement = new ActivityManagement();
+
+        bool exit = false;
+        while (!exit)
+        {
+            AnsiConsole.MarkupLine("[bold cyan]\nHi there! Please choose an option:[/]");
+            var choice = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("[bold green]Select an option:[/]")
+                    .AddChoices(new[]
+                    {
+                        "Log Activity",
+                        "View Reminders",
+                        "Manage Plants",
+                        "View History",
+                        "Exit"
+                    }));
+
+            switch (choice)
             {
-                Console.WriteLine("Login failed. Exiting application.");
-                return;
+                case "Log Activity":
+                    activityManagement.LogActivity(plantsManagement.LoadPlants());
+                    break;
+                case "Manage Plants":
+                    plantsManagement.ManagePlants();
+                    break;
+                case "Exit":
+                    AnsiConsole.MarkupLine("[bold yellow]You entered option to Exit.[/]");
+                    exit = true;
+                    break;
+                default:
+                    AnsiConsole.MarkupLine("[bold red]Invalid entry. Try again.[/]");
+                    break;
             }
+        }
 
-            GardenTracker gardenTracker = new GardenTracker();
-            bool exit = false;
-
-            while (!exit)
-            {
-                Console.WriteLine("\nHi there! Please choose an option:");
-                Console.WriteLine("1. Log Activity");
-                Console.WriteLine("2. View Reminders");
-                Console.WriteLine("3. Manage Plants");
-                Console.WriteLine("4. View History");
-                Console.WriteLine("5. Exit");
-
-                string choice = Console.ReadLine();
-
-                switch (choice)
-                {
-                    case "1":
-                        gardenTracker.LogActivity();
-                        break;
-                    case "2":
-                        gardenTracker.ViewReminders();
-                        break;
-                    case "3":
-                        gardenTracker.ManagePlants();
-                        break;
-                    case "4":
-                        gardenTracker.ViewHistory();
-                        break;
-                    case "5":
-                        Console.WriteLine("You entered option to Exit");
-                        exit = true;
-                        break;
-                    default:
-                        Console.WriteLine("Invalid entry. Try again.");
-                        break;
-                }
-            }
-
-            Console.WriteLine("Thank you, Bye!");
+        AnsiConsole.MarkupLine("[bold green]Thank you, Bye![/]");
     }
 }
