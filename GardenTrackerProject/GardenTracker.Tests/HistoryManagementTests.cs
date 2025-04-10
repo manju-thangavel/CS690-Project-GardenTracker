3dd002fbcff8b98b98c021e3cbb63f16b29c3e99
manju-thangavel <srimanjunath.thangavel@hotmail.com>
2025-04-10 23:34:05 +0000

    commit changes

--- GardenTrackerProject/GardenTracker.Tests/HistoryManagementTests.cs
+++ GardenTrackerProject/GardenTracker.Tests/HistoryManagementTests.cs
@@ -24,26 +24,6 @@ namespace GardenTracker.Tests
         }
 
         [Fact]
-        public void GetHistory_ShouldReturnActivitiesForSpecificPlant_WhenActivitiesExist()
-        {
-            File.WriteAllText(TestActivitiesFilePath, JsonSerializer.Serialize(new List<Activity>
-            {
-                new Activity(new Plant("Tomato"), ActivityType.Watering, DateTime.UtcNow.AddDays(-1), "Watered the plant."),
-                new Activity(new Plant("Tomato"), ActivityType.Pruning, DateTime.UtcNow.AddDays(-2), "Pruned the plant."),
-                new Activity(new Plant("Basil"), ActivityType.Watering, DateTime.UtcNow.AddDays(-1), "Watered the basil.")
-            }));
-
-            var historyManagement = new HistoryManagement(_activityService);
-            var plant = new Plant("Tomato");
-
-            var activities = historyManagement.GetHistory(plant, 3);
-
-            Assert.Equal(2, activities.Count);
-            Assert.Contains(activities, a => a.Note == "Watered the plant.");
-            Assert.Contains(activities, a => a.Note == "Pruned the plant.");
-        }
-
-        [Fact]
         public void GetHistory_ShouldReturnEmptyList_WhenNoActivitiesExistForPlant()
         {
             File.WriteAllText(TestActivitiesFilePath, JsonSerializer.Serialize(new List<Activity>
